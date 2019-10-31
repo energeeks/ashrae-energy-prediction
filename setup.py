c6793788519725291536fdbc7777baec5a9d80da
Tobias Weber <t.weber@campus.lmu.de>
2019-10-31 16:08:06 +0100

    Initialized cookiecutter framework

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Predict Energy Consumption of Buildings with Ease',
+    author='EnerGeeks',
+    license='MIT',
+)
