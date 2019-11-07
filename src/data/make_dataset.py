ffd83b9665d6a68fb43c1b70ed486769223ff86c
Tobias Weber <tobias.weber.1@web.de>
2019-11-07 17:29:13 +0100

    Started dev. makefile + pipeline combo

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -3,6 +3,34 @@ import click
 import logging
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
+import pandas as pd
+import pickle
+
+
+def load_raw_data(input_filepath):
+    """
+    Loads data from .csv files and performs necessary joins. Function returns
+    the training as well as the test set.
+    """
+
+    click.echo("Loading csv files to memory...")
+    building_df = pd.read_csv(input_filepath + "/building_metadata.csv")
+    weather_train = pd.read_csv(input_filepath + "/weather_train.csv")
+    train = pd.read_csv(input_filepath + "/train.csv")
+    weather_test = pd.read_csv(input_filepath + "/weather_test.csv")
+    test = pd.read_csv(input_filepath + "/test.csv")
+
+    click.echo("Performing join operations...")
+    train = train.merge(building_df, left_on="building_id",
+                        right_on="building_id", how="left")
+    train = train.merge(weather_train, left_on=["site_id", "timestamp"],
+                        right_on=["site_id", "timestamp"])
+    test = test.merge(building_df, left_on="building_id",
+                      right_on="building_id", how="left")
+    test = test.merge(weather_test, left_on=["site_id", "timestamp"],
+                      right_on=["site_id", "timestamp"])
+    click.echo("Load successful!")
+    return train, test
 
 
 @click.command()
@@ -14,6 +42,8 @@ def main(input_filepath, output_filepath):
     """
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
+
+    df_train, df_test = load_raw_data(input_filepath)
 
 
 if __name__ == '__main__':
