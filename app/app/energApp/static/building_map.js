30993b73b2efba263d77285989a7eac966444bdc
Tobias Weber <tobias.weber.1@web.de>
2020-01-20 15:44:50 +0100

    fixed map loading

--- app/app/energApp/static/building_map.js
+++ app/app/energApp/static/building_map.js
@@ -10,11 +10,25 @@ let searchControl = new L.esri.Controls.Geosearch().addTo(mymap);
 
 let results = new L.LayerGroup().addTo(mymap);
 
+let marker = L.marker([48.137154, 11.576124], {draggable:'true'});
+results.addLayer(marker);
+
 searchControl.on('results', function(data){
     results.clearLayers();
-    results.addLayer(L.marker(data.results[0].latlng, {draggable:'true'}));
+    marker = L.marker(data.results[0].latlng, {draggable:'true'})
+    results.addLayer(marker);
 
     document.getElementById('latitude').value = data.results[0].latlng.lat;
     document.getElementById('longitude').value = data.results[0].latlng.lng;
     document.getElementById('name').value = data.results[0].address;
 });
+
+marker.on('dragend', function(event) {
+    let result = event.target.getLatLng();
+    document.getElementById('latitude').value = result.lat;
+    document.getElementById('longitude').value = result.lng;
+});
+
+$('#buildingModal').on('shown.bs.modal', function(){
+    mymap.invalidateSize();
+ });
